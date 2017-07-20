1425df2f8c6bc0eba59f6e03c4896d6f77e9097b
liyuhong <liyuhongjob@163.com>
2017-07-20 16:38:58 +0800

    Initial Commit

---
+++ Architecture_Factory_LBS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Architecture_Factory_LBS
+//
+//  Created by ThomsonTang on 2017/7/20.
+//  Copyright © 2017年 ThomsonTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
